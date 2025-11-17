76d286e6b0a7c4aeea962fc02735a6a321372d6e
Nicole Trindade <nicole.trindade@poli.ufrj.br>
2025-11-16 23:34:41 -0300

    funcao comprar acao

--- script.js
+++ script.js
@@ -2,33 +2,47 @@ import {acoes} from './acoes.js';
 
 let acoesdisponiveisparacompra = document.getElementById("acoesdisponiveisparacompra")
 let portfoliodeacoes= document.getElementById("portfoliodeacoes")
+
 function CarregarAcoes() {
 
     for(let i=0; i<acoes.length; i++){
-        
+        let acaodisponivel = document.createElement("div")
+        acaodisponivel.setAttribute('id', 'acao' + i ) 
+        acaodisponivel.setAttribute('class', 'acaodisponivel')
+        acoesdisponiveisparacompra.append(acaodisponivel)
+
         let acaocodigo = document.createElement("p")
         acaocodigo.textContent = acoes[i]["codigo"]
+        acaocodigo.setAttribute('id', acoes[i]["codigo"]) 
+        acaocodigo.setAttribute('class', 'acaocodigo')     
+        acaocodigo.setAttribute('value', acoes[i]["codigo"])    
+
         let acaopreco = document.createElement("p")
         acaopreco.textContent = acoes[i]["preco"]
-        let botaocompraracao = document.createElement("button")
+        acaopreco.setAttribute('class', 'acaopreco') 
+        acaopreco.setAttribute('value', acoes[i]["preco"])
+
+        let botaocompraracao = document.createElement('button')
+        botaocompraracao.setAttribute('type', 'button' )
+        botaocompraracao.textContent = 'comprar acao'
+        botaocompraracao.addEventListener("click", function (){ ComprarAcao(acaocodigo.id)})      
+
         
-        botaocompraracao.textContent = "comprar acao"
-        botaocompraracao.setAttribute("id", "comprou " + acoes[i]["codigo"])
-        acoesdisponiveisparacompra.append(acaocodigo)
-        acoesdisponiveisparacompra.append(acaopreco)
-        acoesdisponiveisparacompra.append(botaocompraracao)
+        acaodisponivel.append(acaocodigo)
+        acaodisponivel.append(acaopreco)
+        acaodisponivel.append(botaocompraracao)
     }
 }
 CarregarAcoes()
 
-//document.getElementsByTagName("button").onclick = function ComprarAcao (event){
-//    event.preventDefault()
-//    console.log(button.id)
-//}
+function ComprarAcao(codigodaacao){
 
-acoesdisponiveisparacompra.addEventListener('click', function(e) {
-    let acaoadquirida= document.createElement("p")
-    acaoadquirida.textContent=(e.target.id);
-    portfoliodeacoes.append(acaoadquirida)
+    let comprouacao = document.createElement("p")
+    comprouacao.textContent = "Voce comprou " + codigodaacao
+    portfoliodeacoes.append(comprouacao)
 
-});
+}
+
+
+
+
